7a362c591367cb271583c58f41d8088920c067bd
Vaimer9 <vaim3r9@gmail.com>
2026-09-07 02:12:57 +0530

    Added formatting to logging

--- include/log.hpp
+++ include/log.hpp
@@ -1,11 +1,48 @@
 #pragma once
 
-#include <string>
+#include <cstdio>
+#include <print>
+#include <format>
+#include <utility>
 
 namespace scrwl
 {
-    void log_success(std::string msg);
-    void log_warn(std::string msg);
-    void log_err(std::string msg);
-    void log_panic(std::string msg);
+    template<typename... Args>
+    void log_info(std::format_string<Args...> fmt, Args&&... args)
+    {
+        std::println(
+            "\033[1;32m[INFO]: \033[0m{}",
+            std::format(fmt, std::forward<Args>(args)...)
+        );
+    }
+
+    template<typename... Args>
+    void log_warn(std::format_string<Args...> fmt, Args&&... args)
+    {
+        std::println(
+            "\033[1;33m[WARN]: \033[0m{}",
+            std::format(fmt, std::forward<Args>(args)...)
+        );
+    }
+
+    template<typename... Args>
+    void log_err(std::format_string<Args...> fmt, Args&&... args)
+    {
+        std::println(
+            stderr,
+            "\033[1;31m[ERR]: \033[0m{}",
+            std::format(fmt, std::forward<Args>(args)...)
+        );
+    }
+
+    template<typename... Args>
+    void log_panic(std::format_string<Args...> fmt, Args&&... args)
+    {
+        std::println(
+            stderr,
+            "\033[1;31m[PANIC]: \033[0m{}",
+            std::format(fmt, std::forward<Args>(args)...)
+        );
+        std::exit(-1);
+    }
 }
---
+++ include/scrwl.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "log.hpp"
+#include "site.hpp"
